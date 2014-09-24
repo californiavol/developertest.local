ae7546c80f29ae4a3785480c6891d79398ab2c1a
californiavol <charlesbrownroberts@gmail.com>
2014-09-24 16:29:02 -0700

    added router to Person module.config.php

--- module/Person/config/module.config.php
+++ module/Person/config/module.config.php
@@ -1,3 +1,57 @@
 <?php
 return array(
+    'controllers' => array(
+        'invokables' => array(
+            'Person\Controller\Index' => 'Person\Controller\IndexController',
+        ),
+    ),
+    'router' => array(
+        'routes' => array(
+            'home' => array(
+                'type' => 'Zend\Mvc\Router\Http\Literal',
+                'options' => array(
+                    'route'    => '/',
+                    'defaults' => array(
+                        'controller' => 'Person\Controller\Index',
+                        'action'     => 'index',
+                    ),
+                ),
+            ),
+            // The following is a route to simplify getting started creating
+            // new controllers and actions without needing to create a new
+            // module. Simply drop new controllers in, and you can access them
+            // using the path /application/:controller/:action
+            'application' => array(
+                'type'    => 'Literal',
+                'options' => array(
+                    'route'    => '/person',
+                    'defaults' => array(
+                        '__NAMESPACE__' => 'Person\Controller',
+                        'controller'    => 'Index',
+                        'action'        => 'index',
+                    ),
+                ),
+                'may_terminate' => true,
+                'child_routes' => array(
+                    'default' => array(
+                        'type'    => 'Segment',
+                        'options' => array(
+                            'route'    => '/[:controller[/:action]]',
+                            'constraints' => array(
+                                'controller' => '[a-zA-Z][a-zA-Z0-9_-]*',
+                                'action'     => '[a-zA-Z][a-zA-Z0-9_-]*',
+                            ),
+                            'defaults' => array(
+                            ),
+                        ),
+                    ),
+                ),
+            ),
+        ),
+    ),
+    'view_manager' => array(
+        'template_path_stack' => array(
+            'person' => __DIR__ . '/../view',
+        ),
+    ),
 );
